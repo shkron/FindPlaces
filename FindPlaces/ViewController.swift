39074e986b2d9907fc39b793e9347db1b1df37db
Alexey <emelyanoff@gmail.com>
2017-03-28 20:42:36 -0700

    Initial Commit

---
+++ FindPlaces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FindPlaces
+//
+//  Created by Alex on 3/28/17.
+//  Copyright © 2017 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
